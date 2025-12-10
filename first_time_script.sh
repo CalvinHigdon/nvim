2f4bb08c1ee087b0cece0366aacf8b45b3fe1e4b
Calvin Higdon <calvindhigdon@Gmail.com>
2025-12-10 13:11:17 -0500

    Update first_time_script.sh

---
+++ first_time_script.sh
@@ -0,0 +1,6 @@
+mkdir -p ~/.local/share/fonts/DroidSansM
+cd ~/.local/share/fonts/DroidSansM && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFontMono-Regular.otf
+
+mkdir -p ../CaskaydiaCode
+cd ../CaskaydiaCode && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/CascadiaCode/CaskaydiaCoveNerdFont-Regular.ttf
+
